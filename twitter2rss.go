711da58bad38e61b75a5ecf7114383bfbdd09baf
Volker <lists.volker@gmail.com>
2016-01-20 21:52:18 -0500

    Created webservice and fix URL & date

--- twitter2rss.go
+++ twitter2rss.go
@@ -6,15 +6,17 @@ import (
     "log"
     "os"
     "time"
+    "net/http"
 
     "github.com/coreos/pkg/flagutil"
     "github.com/dghubble/go-twitter/twitter"
     "github.com/dghubble/oauth1"
-	  "github.com/davecgh/go-spew/spew"
+    "github.com/davecgh/go-spew/spew"
     "github.com/gorilla/feeds"
 )
 
-func main() {
+
+func get_rss() string {
     flags := flag.NewFlagSet("user-auth", flag.ExitOnError)
     consumerKey := flags.String("consumer-key", "", "Twitter Consumer Key")
     consumerSecret := flags.String("consumer-secret", "", "Twitter Consumer Secret")
@@ -43,30 +45,26 @@ func main() {
     feed := &feeds.Feed{
       Title:       "Twitter Home Timeline",
       Description: "Twitter Home Timeline RSS Feed",
-      Author:      &feeds.Author{"My name", "my@email"},
-      Link:        &feeds.Link{Href: "http://notyetdefined/"},
+      Author:      &feeds.Author{"Twitter2RSS", "lists.volker@gmail.com"},
+      Link:        &feeds.Link{Href: "http://github.com:volker-fr/twitter2rss/"},
       Created:     now,
     }
     feed.Items = []*feeds.Item{}
 
     // Get timeline
-    homeTimelineParams := &twitter.HomeTimelineParams{Count: 5}
+    homeTimelineParams := &twitter.HomeTimelineParams{Count: 50}
     tweets, _, _ := client.Timelines.HomeTimeline(homeTimelineParams)
 
     for _,tweet := range tweets {
-        if *debug {
-          spew.Dump(tweet)
-        }
+        if *debug { spew.Dump(tweet) }
 
-        const longForm = "Mon Jan 11 02:58:57 +0000 2016"
-        t, _ := time.Parse(longForm, tweet.CreatedAt)
-        fmt.Println(tweet.CreatedAt)
-        fmt.Println(t)
+        t, _ := time.Parse(time.RubyDate, tweet.CreatedAt)
 
+        url := "https://twitter.com/" + tweet.User.ScreenName + "/status/" + tweet.IDStr
 
         item := &feeds.Item{
           Title:       fmt.Sprintf("%s: %s...", tweet.User.Name, tweet.Text[:10] ),
-          Link:        &feeds.Link{Href: "http://tobedefined"},
+          Link:        &feeds.Link{Href: url},
           Description: tweet.Text,
           Author:      &feeds.Author{tweet.User.Name, tweet.User.ScreenName},
           Created:     t,
@@ -81,7 +79,16 @@ func main() {
         log.Fatal(err)
     }
 
-    fmt.Println(atom, "\n")
-//spew.Dump(feed)
+    if *debug { fmt.Println(atom, "\n") }
 
+    return atom
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "%s", get_rss())
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
